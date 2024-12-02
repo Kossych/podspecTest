c426ad052b4a6bad6e2a776a724ce8a2bee4037a
Artem <akosov@samokat.ru>
2024-12-02 20:42:22 +0700

    test

---
+++ events.swift
@@ -0,0 +1,69 @@
+//description
+class Event {
+    var value: String = "sampleValue"
+    init(value: String) {
+        this.value = value
+    }
+}
+
+//desc
+class TEST {
+    var Test: test? = null
+    init(Test: test) {
+        this.Test = Test
+    }
+
+}
+
+class test {
+    var Govno: govno? = null
+    var Object: object? = null
+    init(Govno: govno, Object: object) {
+        this.Govno = Govno
+        this.Object = Object
+    }
+
+}
+
+class govno {
+    var path: String? = null
+    init(path: String) {
+        this.path = path
+    }
+
+}
+
+class object {
+    var testDouble: String = "string"
+    var testTesta: String = "testTesta"
+    init(testDouble: String, testTesta: String) {
+        this.testDouble = testDouble
+        this.testTesta = testTesta
+    }
+}
+
+//ddd
+class Main Page {
+    var User_properties: user_properties? = null
+    init(User_properties: user_properties) {
+        this.User_properties = User_properties
+    }
+
+}
+
+class user_properties{
+    var int_number: Int
+    var string_atr: String
+    init(int_number: Int, string_atr: String) {
+        this.int_number = int_number
+        this.string_atr = string_atr
+    }
+}
+
+//description
+class TEST{
+    var value: String = "dasdsadsad"
+    init(value: String) {
+        this.value = value
+    }
+}
